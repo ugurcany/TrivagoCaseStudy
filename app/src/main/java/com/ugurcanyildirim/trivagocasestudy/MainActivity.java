87e1001aa499167c9937ac1f6a3ec7263fff6a2d
Ugurcan Yildirim <ugurcany@outlook.com>
2016-08-11 00:32:56 +0300

    SearchView added to UI

--- app/src/main/java/com/ugurcanyildirim/trivagocasestudy/MainActivity.java
+++ app/src/main/java/com/ugurcanyildirim/trivagocasestudy/MainActivity.java
@@ -2,10 +2,17 @@ package com.ugurcanyildirim.trivagocasestudy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.arlib.floatingsearchview.FloatingSearchView;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.searchView)
+    FloatingSearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,7 +20,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        searchView.setOnQueryChangeListener(new FloatingSearchView.OnQueryChangeListener() {
+            @Override
+            public void onSearchTextChanged(String oldQuery, final String newQuery) {
 
+                Log.d(MainActivity.class.getSimpleName(), "Search: " + newQuery);
+
+            }
+        });
 
     }
 }
