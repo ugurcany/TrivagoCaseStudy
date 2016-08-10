c05c9487f3defc37306dfea058f31ad7a5f5a769
Ugurcan Yildirim <ugurcan.yildirim@accenture.com>
2016-08-10 16:39:22 +0300

    Toolbar implemented & added to activity

--- app/src/main/java/com/ugurcanyildirim/trivagocasestudy/MainActivity.java
+++ app/src/main/java/com/ugurcanyildirim/trivagocasestudy/MainActivity.java
@@ -3,11 +3,17 @@ package com.ugurcanyildirim.trivagocasestudy;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+
+
     }
 }
